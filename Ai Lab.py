30464c0e4838449054f3280805a968705bd73c0d
Md Limon <21100002@IIML.Local>
2022-02-15 10:08:40 +0600

    matplotlib add

--- Ai Lab.py
+++ Ai Lab.py
@@ -1,4 +1,9 @@
 import  pandas as pd
-
+import matplotlib.pyplot as plt
 patient  = pd.read_csv("./data-set/indian_liver_patient.csv")
-print(patient.head())
+# print(patient.head())
+Total_Bilirubin = patient["Total_Bilirubin"].value_counts(sort=False)
+print(Total_Bilirubin)
+Total_Bilirubin.plot(title="Total_Bilirubin of players per  year")
+plt.show()
+
